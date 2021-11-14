90a9dec4ecba12750246f8351457f0037a6b4be1
Tegar Pratama <tegar.yokesen@gmail.com>
2021-11-14 11:26:03 +0700

    compete api

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,6 +2,9 @@ const Item = require('../models/item');
 const Treasure = require('../models/Activity');
 const Traveler = require('../models/Booking');
 const Category = require('../models/Category');
+const Bank = require('../models/Bank');
+const Booking = require('../models/Booking');
+const Member = require('../models/Member');
 
 module.exports = {
    landingPage: async (req, res) => {
@@ -67,5 +70,115 @@ module.exports = {
          console.log(err);
          res.status(500).json({ message: 'Internal server error'});
       }
-   }
+   },
+
+   detailPage: async (req, res) => {
+      try {
+         const { id } = req.params;
+         const item = await Item.findOne({ _id: id })
+            .populate({ path: 'featureId', select: '_id name qty imageUrl' })
+            .populate({ path: 'activityId', select: '_id name type imageUrl' })
+            .populate({ path: 'imageId', select: '_id imageUrl'});
+
+         const bank = await Bank.find();
+         const testimonial = {
+            _id: "asd123123",
+            imageUrl: 'images/testimonial1.jpg',
+            name: 'Happy Family',
+            rate: 4.55,
+            content: 'Tesating',
+            familyName: 'Tegar',
+            familyOccupation: 'Pengusaha'
+         }
+
+         res.status(200).json({
+            ...item._doc,
+            bank,
+            testimonial
+         });
+      } catch (err) {
+         console.log(err);
+         res.status(500).json({ message: 'Internal server error'});
+      }
+   },
+
+   bookingPage: async (req, res) => {
+      const {
+         idItem, 
+         duration,
+         price,
+         bookingStartDate,
+         bookingEndDate,
+         firstName,
+         lastName,
+         email,
+         phoneNumber,
+         accountHolder,
+         bankFrom,
+      } = req.body;
+
+      if (!req.file) {
+         res.status(404).json({ message: 'Image not found' });
+      }
+
+      if (
+         idItem === undefined ||
+         duration === undefined ||
+         bookingStartDate === undefined ||
+         bookingEndDate === undefined ||
+         firstName === undefined ||
+         lastName === undefined ||
+         email === undefined ||
+         phoneNumber === undefined ||
+         accountHolder === undefined ||
+         bankFrom === undefined
+      ) {
+         res.status(404).json({ message: 'Lengkapi semua field' });
+      }
+
+      const item = await Item.findOne({ _id: idItem });
+
+      if (!item) {
+         res.status(404).json({ message: 'Item not found' });
+      }
+
+      item.sumBooking += 1;
+      await item.save();
+
+      let total = item.price * duration;
+      let tax = total * 0.1;
+
+      const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+      const member = await Member.create({
+         firstName,
+         lastName,
+         email,
+         phoneNumber
+      });
+
+      const newBooking = {
+         invoice,
+         bookingStartDate,
+         bookingEndDate,
+         total: total += tax,
+         itemId: {
+            _id: item.id,
+            title: item.title,
+            price: item.price,
+            duration: duration
+         },
+
+         memberId: member.id,
+         payments: {
+            proofPayment: `images/${req.file.filename}`,
+            bankFrom: bankFrom,
+            accountHolder: accountHolder
+         }
+      }
+
+      const booking = await Booking.create(newBooking);
+
+      res.status(201).json({ message: 'Success Booking', booking });
+   },
 }
--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const apiController = require('../controllers/apiController');
 
 // Define middleware
-// const { upload, uploadMultiple } = require('../middlewares/multer');
+const { upload, uploadMultiple } = require('../middlewares/multer');
 // const auth = require('../middlewares/auth');
 
 router.get('/landing-page', apiController.landingPage);
+router.get('/detail-page/:id', apiController.detailPage);
+router.post('/booking-page', upload, apiController.bookingPage);
 
 module.exports = router;
